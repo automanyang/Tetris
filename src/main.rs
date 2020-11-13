ae9e8ba62db5f56f1f3d46e5bf5212a5a36ccf06
automanyang <automanyang@gmail.com>
2020-11-13 14:21:41 +0800

    simple the code

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,4 @@ fn main() {
     let app = fltk::app::App::default();
     let _tetris = tetris::TetrisWindow::init();
     app.run().unwrap();
-    // while app.wait() {
-    //     let k = fltk::app::event_key();
-    //     dbg!(&k);
-    //     let s = fltk::app::event_text();
-    //     dbg!(&s);
-    // }
 }
